7f6967a7a9465f2a7d7d9f03546965fad019b33a
mazlan.ibrahim <mazlan.ibrahim@uberfusion.com>
2023-04-14 10:46:20 +0800

    chapter 6.2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature = 'recipe';
+
   title = 'ng4-complete-guide';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
